4dfb541ccf187387af1f8c678b29d7809f1f99c4
Kun He <kun.he@sjsu.edu>
2018-04-29 18:48:03 -0700

    Initial Test

---
+++ Mancala/src/DataModel.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class DataModel {
+
+	final int MAX_PER_PIT = 4;
+	
+	Map<String, Integer> pits;
+	
+	public DataModel()
+	{
+		pits = new HashMap<String, Integer>();
+	}
+	
+	public void add(String pit, int amount)
+	{
+		
+	}
+	
+	public void remove(String pit, int amount)
+	{
+		
+	}
+}
