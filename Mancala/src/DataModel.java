1b1ce8b3787015b32955950e92e7aa43db1a8c48
MartinRoom <kun.he@sjsu.edu>
2018-04-29 18:59:51 -0700

    add and remove

--- Mancala/src/DataModel.java
+++ Mancala/src/DataModel.java
@@ -13,11 +13,23 @@ public class DataModel {
 	
 	public void add(String pit, int amount)
 	{
+		String temp = pit.toUpperCase();
 		
+		if(temp.contains("AB"))
+		{
+			pits.put(temp, amount);
+			return;
+		}
+		
+		if(amount <= MAX_PER_PIT)
+			pits.put(temp, amount);
+			
+
 	}
 	
 	public void remove(String pit, int amount)
 	{
-		
+		if(amount < pits.get(pit.toUpperCase()))
+			pits.put(pit.toUpperCase(), amount);
 	}
 }
