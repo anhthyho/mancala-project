222e200fc1b96eb77a533fcd3acb82f94830c4b6
Steven V <7stevenvuong@gmail.com>
2018-04-30 23:31:23 -0700

    strategies!

---
+++ Mancala/src/GUIViewStrategy.java
@@ -0,0 +1,11 @@
+
+public class GUIViewStrategy implements ViewStrategy
+{
+
+	@Override
+	public void display(DataModel dataModel) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Mancala/src/ViewStrategy.java
@@ -0,0 +1,5 @@
+
+public interface ViewStrategy 
+{
+	public void display(DataModel dataModel);
+}
